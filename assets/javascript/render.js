a76102b559a45839e25298efa19ab21e481255d4
yrllanbrandao <yrllanbrandao@outlook.com>
2023-05-22 10:16:31 -0300

    feat: parse html to markdown and render

--- assets/javascript/render.js
+++ assets/javascript/render.js
@@ -1,4 +1,32 @@
 import {factory} from './factory.js';
+function convertHtmlToMarkdown(html) {
+    const containerRegex = /<div class="container-fluid content-row">([^]+?)<\/div>/;
+    const labelRegex = /<h2 class="text-2">([^<]+)<\/h2>/;
+    const hrRegex = /<hr>/;
+    const descriptionRegex = /<p class="text-simple">([^<]+)<\/p>/;
+    const methodRegex = /<h2 class="text-2">Method<\/h2>\s*<li class="text-simple">([^<]+)<\/li>/;
+    const routeRegex = /<h2 class="text-2">Route<\/h2>\s*<li class="text-simple">([^<]+)<\/li>/;
+  
+    const containerMatch = html.match(containerRegex);
+    if (!containerMatch) {
+      return '';
+    }
+    const [, containerContent] = containerMatch;
+  
+    const labelMatch = containerContent.match(labelRegex);
+    const label = labelMatch ? labelMatch[1] : '';
+    const hr = containerContent.match(hrRegex) ? '' : '';
+    const descriptionMatch = containerContent.match(descriptionRegex);
+    const description = descriptionMatch ? descriptionMatch[1] : '';
+    const methodMatch = containerContent.match(methodRegex);
+    const method = methodMatch ? methodMatch[1] : '';
+    const routeMatch = containerContent.match(routeRegex);
+    const route = routeMatch ? routeMatch[1] : '';
+  
+    return `## ${label}\n${hr}\n\n${description}\n\n**Method**\n\n${method}\n\n**Route**\n\n${route}\n`;
+  }
+
+
 export function render()
 {
     const className = ['container-fluid','content-row']
@@ -7,18 +35,28 @@ export function render()
     const description = document.getElementById("description").value;
     const method = document.getElementById("method").value;
     const target = document.getElementById("result-html");
+    const targetMD = document.getElementById("result-markdown");
     const block = factory('div', className);
 
-    block.innerHTML += `<h2 class="text-2">${label}</h2> `;
-    block.innerHTML += !!label ? `<hr/>`: null;
-    block.innerHTML += `<p class="text-simple">${description}</p>`
+    const htmlBlock = `
+        <div class="container-fluid content-row">
+        <h2 class="text-2">${label}</h2>
+        ${label ? '<hr>' : ''}
+        <p class="text-simple">${description}</p>
+        <h2 class="text-2">Method</h2>
+        <li class="text-simple">${method}</li>
+        <h2 class="text-2">Route</h2>
+        <li class="text-simple">${route}</li>
+        </div>
+        `;
 
-    block.innerHTML += `<h2 class="text-2">Method</h2> `;
-    block.innerHTML += ` <li class="text-simple"> ${method}</li>`
 
-    block.innerHTML += `<h2 class="text-2"> Route</h2>`
-    block.innerHTML += ` <li class="text-simple"> ${route}</li>`
+        
+    block.innerHTML += htmlBlock;
+    targetMD.innerHTML += convertHtmlToMarkdown(htmlBlock)
+
     target.append(block);
-   
+
+    console.log(targetMD.innerHTML)
     
 }
